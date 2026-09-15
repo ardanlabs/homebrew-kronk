dbc4bf7418def70283e60850ff6b2004c0b78004
Matteo Marchelli <matteo.marchelli@gmail.com>
2026-09-15 14:19:35 +0100

    [chore] change deprecated postflight to postflight_steps

--- Casks/kronk.rb
+++ Casks/kronk.rb
@@ -33,7 +33,7 @@ cask "kronk" do
 
   binary "kronk"
 
-  postflight do
+  postflight_steps do
     if OS.mac?
       system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{staged_path}/kronk"]
     end
